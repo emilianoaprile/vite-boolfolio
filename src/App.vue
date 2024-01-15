44d8cc39e794deb5e93a8896bd2ef08b5c5817b6
EmilianoAprile <emilianoaprile77@icloud.com>
2024-01-15 16:38:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      projects:[]
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>Ciao Mondo</div>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss"
+
+
+</style>
